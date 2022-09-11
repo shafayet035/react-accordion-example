4c76277be254844b6d1a73dd58fc7f994a6d7f2c
shafayet035 <shafayet035@gmail.com>
2022-09-11 15:44:12 +0600

    Added style, Testing and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./components/Accordion";
+
+const demoData = [
+  {
+    title: "This is a title",
+    body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint quos dignissimos, molestias ut ipsam excepturi! Quidem ad dolores, eum nisi maxime, facilis rerum, laborum architecto culpa corporis minima. Inventore nisi ducimus exercitationem. Quas ipsum et iure consequatur expedita, nobis accusamus sit hic sed earum delectus consectetur natus soluta incidunt.",
+  },
+  {
+    title: "This is a title",
+    body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint quos dignissimos, molestias ut ipsam excepturi! Quidem ad dolores, eum nisi maxime, facilis rerum, laborum architecto culpa corporis minima. Inventore nisi ducimus exercitationem. Quas ipsum et iure consequatur expedita, nobis accusamus sit hic sed earum delectus consectetur natus soluta incidunt.",
+  },
+  {
+    title: "This is a title",
+    body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint quos dignissimos, molestias ut ipsam excepturi! Quidem ad dolores, eum nisi maxime, facilis rerum, laborum architecto culpa corporis minima. Inventore nisi ducimus exercitationem. Quas ipsum et iure consequatur expedita, nobis accusamus sit hic sed earum delectus consectetur natus soluta incidunt.",
+  },
+  {
+    title: "This is a title",
+    body: (
+      <>
+        <h1>This is title of the Body</h1>
+        <p>Hello World</p>
+      </>
+    ),
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Accordion data={demoData} defaultExpandItem={2} />
     </div>
   );
 }
